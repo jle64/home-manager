19370f48b4f1a84b56c4f1a268399009c696d91f
Jonathan Lestrelin <jonathan@lestrel.in>
2026-09-17 23:09:28 +0200

    ++

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
     homeConfigurations."jonathan@cloud" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
 
-      modules = [./base.nix ./cli.nix ./desktop.nix { home.homeDirectory = "/var/home/jonathan"; } nix-index-database.homeModules.nix-index];
+      modules = [./base.nix ./cli.nix ./desktop.nix { home.homeDirectory = "/home/jonathan"; } nix-index-database.homeModules.nix-index];
     };
     homeConfigurations."jonathan@sephiroth" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
